4a675618d1da34a05fa33f07de21db1b8cf255fa
rmholsen <jaguar.kitty@gmail.com>
2021-07-19 11:49:31 -0400

    Completed

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -9,12 +9,22 @@ export default class ColorBox extends Component {
   }
 
   render() {
-    return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
-      </div>
-    )
+  // put the if/then directly in the render function
+  // the control structure goes off immediately when the render function is called
+      if(this.props.opacity >= 0.2) {
+        return (
+        // if the opacity is greater than or equal to, etc create the color box in HTML and then 
+        // call the ColorBox JS again feeding a lower opacity into it
+        // THIS CREATES A RECURSIVE FUNCTION. DO NOT FORGET THE BREAK CONDITION. LACK OF BREAK CONDITIONS IS BAD. 
+        <div className="color-box" style={{opacity: this.props.opacity}}>
+          <ColorBox opacity={this.props.opacity - 0.1} />
+        </div>
+        )
+      } else {
+        return null 
+    }
   }
 
+  // end ColorBox 
 }
 
